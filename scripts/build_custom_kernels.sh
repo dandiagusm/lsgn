7875cf485328c966e29abffa26b2772c6fca4458
Dandi <13517077@std.stei.itb.ac.id>
2020-11-02 23:33:06 +0700

    edit config

--- scripts/build_custom_kernels.sh
+++ scripts/build_custom_kernels.sh
@@ -9,7 +9,7 @@ TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 # On machines with gcc>=5.0
 #assume most people will be running >5
-g++ -std=c++11 -shared srl_kernels.cc -o srl_kernels.so -I$TF_INC -I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 -shared srl_kernels.cc -o srl_kernels.so -I$TF_INC -I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework -fPIC -O2
 
 # Mac
 #g++ -std=c++11 -shared coref_kernels.cc -o coref_kernels.so -I $TF_INC -fPIC -D_GLIBCXX_USE_CXX11_ABI=0  -undefined dynamic_lookup
